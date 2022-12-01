7239d41d5d973b82a660e72e4d63d852d24fabdf
OndrejPech <pech.ondrej@gmail.com>
2022-12-01 20:50:40 +0100

    day 1 complete

--- 2022/day 01/solutions.py
+++ 2022/day 01/solutions.py
@@ -1,1 +1,22 @@
-print('Lets do this')
+with open('input.txt', 'r') as file:
+    elve_num = 1
+    all_elves = {}
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        line = line.rstrip()
+
+        if line == '':
+            elve_num += 1
+        else:
+            all_elves[elve_num] = all_elves.get(elve_num, 0) + int(line)
+
+
+# PART 1
+highest = max(all_elves.values())
+print(highest)
+
+# PART 2
+top_three = sorted(all_elves.values(), reverse=True)[:3]
+print(sum(top_three))
